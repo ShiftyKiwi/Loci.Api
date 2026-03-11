2fc91b57ac968ff13bf134e7bdecbd64dea226f5
Cordelia Mist <buzzmodetrix@gmail.com>
2026-03-11 16:41:00 -0700

    updeate appy type

---
+++ Enums/PresetApplyType.cs
@@ -0,0 +1,22 @@
+namespace LociApi.Enums;
+
+/// <summary>
+///   Dictates how a preset should be applied to an ActorSM
+/// </summary>
+public enum PresetApplyType
+{
+    /// <summary>
+    ///   Any existing statuses that are already active are ignored.
+    /// </summary>
+    IgnoreExisting = 0,
+
+    /// <summary>
+    ///   Existing statuses have their timer and stack counts reset on application.
+    /// </summary>
+    UpdateExisting = 1,
+
+    /// <summary>
+    ///   All other statuses are removed prior to application. <b>Use with caution.</b>
+    /// </summary>
+    ReplaceAll 2,
+}
